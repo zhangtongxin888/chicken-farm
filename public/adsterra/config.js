94f56036214af39984e66b10553fd2db19dbb116
zhangtongxin888 <zhangtongxin12@gmail.com>
2026-08-20 06:59:11 +0800

    feat: add Adsterra placements

---
+++ public/adsterra/config.js
@@ -0,0 +1,28 @@
+// adsterra-code-handoff:plain-config
+// Domain: chickenfarmgame.wiki
+export const adsterraConfig = {
+  "adsterraBannerId": "49705446702a3a2bbd70f54f85ae0722",
+  "adsterraBanner300x250Key": "49705446702a3a2bbd70f54f85ae0722",
+  "adsterraBanner300x250ScriptUrl": "https://www.highperformanceformat.com/49705446702a3a2bbd70f54f85ae0722/invoke.js",
+  "adsterraBanner320x50Key": "db4af02c4d378d226a6ace2f7ae73536",
+  "adsterraBanner320x50ScriptUrl": "https://www.highperformanceformat.com/db4af02c4d378d226a6ace2f7ae73536/invoke.js",
+  "adsterraBanner728x90Key": "015ebd318d695b79c52bd2e56d43cde9",
+  "adsterraBanner728x90ScriptUrl": "https://www.highperformanceformat.com/015ebd318d695b79c52bd2e56d43cde9/invoke.js",
+  "adsterraBanner468x60Key": "8d91af9633df570672f182242e3be170",
+  "adsterraBanner468x60ScriptUrl": "https://www.highperformanceformat.com/8d91af9633df570672f182242e3be170/invoke.js",
+  "adsterraBanner160x300Key": "50f07e03405a309322f3ffb3571b9ea2",
+  "adsterraBanner160x300ScriptUrl": "https://www.highperformanceformat.com/50f07e03405a309322f3ffb3571b9ea2/invoke.js",
+  "adsterraBanner160x600Key": "be552c0ce16c15d878fadde34b66f789",
+  "adsterraBanner160x600ScriptUrl": "https://www.highperformanceformat.com/be552c0ce16c15d878fadde34b66f789/invoke.js",
+  "adsterraNative1Id": "container-ab873a97b0f98913df880768fecd6439",
+  "adsterraNative1ScriptUrl": "https://pl30931007.effectivecpmnetwork.com/ab873a97b0f98913df880768fecd6439/invoke.js",
+  "adsterraSmartLinkUrl": "https://www.effectivecpmnetwork.com/q03trr14r8?key=8946ad6238c05d5cd9258146c033c224",
+  "adsterraPopunderScriptUrl": "https://pl30931003.effectivecpmnetwork.com/f1/54/44/f154445a846422b37b2c1750a3668851.js",
+  "adsterraSocialBarScriptUrl": "https://pl30931004.effectivecpmnetwork.com/e3/6c/28/e36c28faff5476db1b5035f623e9eaff.js",
+  "adsterraLeaderboardId": "015ebd318d695b79c52bd2e56d43cde9",
+  "adsterraEnablePopunder": false,
+  "adsterraEnableSocialBar": false,
+  "adsterraEnableStickyRail": false,
+  "adsterraPopunderDelayMs": 30000,
+  "adsterraPopunderMinPageViews": 2
+};
